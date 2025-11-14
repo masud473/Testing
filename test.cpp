56ab0fd370c9b27c6550d833326d3c81ffa149a0
Md. Farhan Masud <112246492+masud473@users.noreply.github.com>
2025-11-14 19:18:58 +0600

    nothing much

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    for (int i = 0; i < 1e7; i++)
+    {
+        cout << i;
+    }
+    cout << "Done";
+    return 0;
+}
